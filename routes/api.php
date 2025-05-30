998027cc0c70c65c40c30c23add790a64595d964
lars <larsa.magnussen@gmail.com>
2025-05-30 10:07:20 +0200

    name routes

--- routes/api.php
+++ routes/api.php
@@ -19,6 +19,6 @@ Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/posts', [PostController::class, 'index']);
-Route::get('posts/{post}', [PostController::class, 'show']);
-Route::post('posts/', [PostController::class, 'store']);
+Route::get('/posts', [PostController::class, 'index'])->name('post.index');
+Route::get('posts/{post}', [PostController::class, 'show'])->name('post.show');
+Route::post('posts/', [PostController::class, 'store'])->name('post.store');
